c1a74d6738e4f9b2c89220e6c6ac6c0f45a0a2e1
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-12 15:50:54 +0100

    docs: app style continue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ export default function Footer() {
 
     // markup
     return (
-        <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis provident ratione perferendis dolorem minima veritatis autem illo incidunt doloribus nam voluptas, dicta adipisci, reiciendis hic culpa quam dolore ex? Est.
-        </p>
+        <footer>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis provident ratione perferendis dolorem minima veritatis autem illo incidunt doloribus nam voluptas, dicta adipisci, reiciendis hic culpa quam dolore ex? Est.
+            </p>
+        </footer>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,20 +3,24 @@ export default function Main() {
     // markup
     return (
         <main>
-            <div className="card">
-                <div className="card-top">
-                    <img src="https://picsum.photos/600/400" alt="immagine casuale" />
-                </div>
-                <div className="card-bottom">
-                    <h4>
-                        Titolo del post
-                    </h4>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis cumque quibusdam a tempora eius labore earum minus. Dolorem quasi ipsa, suscipit qui laboriosam repellendus repellat sit delectus quaerat nihil repudiandae?
-                    </p>
-                    <button>
-                        Leggi di più
-                    </button>
+            <div className="container">
+                <div className="card">
+                    <div className="card-top">
+                        <p>
+                            600x400
+                        </p>
+                    </div>
+                    <div className="card-bottom">
+                        <h4>
+                            Titolo del post
+                        </h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis cumque quibusdam a tempora eius labore earum minus. Dolorem quasi ipsa, suscipit qui laboriosam repellendus repellat sit delectus quaerat nihil repudiandae?
+                        </p>
+                        <button>
+                            Leggi di più
+                        </button>
+                    </div>
                 </div>
             </div>
         </main>
