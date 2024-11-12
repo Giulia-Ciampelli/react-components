667a8a2be25e55f1bef3fa929b635df5d1471c11
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-12 14:42:44 +0100

    docs: header import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-// importazione
+// #region importazione
+
+// stile
 import './App.css'
+
+// componenti
+import Header from './components/Header.jsx'
+// #endregion importazione
 
 function App() {
 
   return (
     <>
-      
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    
+    // logica
+    const title = 'Il mio blog';
+
+    // markup
+    return (
+        <h1>
+            {title}
+        </h1>
+    )
+}
